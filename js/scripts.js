03b4638c5d079a5450e1fff2fd7ae8ff8c97d8a9
neil-c1119 <neil.curtis1119@gmail.com>
2019-09-04 19:22:14 -0700

    Blog pagination function fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,39 @@
 $(document).ready(function() {
-  var realBlog = document.getElementsByClassName("realBlog");
-  //var numberOfPosts = realBlog.length;
+  var $realBlog = $(".realBlog");
+  //var numberOfPosts = $realBlog.length;
   var postsPerPage = 10;
-  var pageCount = Math.ceil(realBlog.length/10);
+  var pageCount = Math.ceil($realBlog.length/10);
   var $pagination = $(".pagination")
 
   function showPage(page) {
-    $(realBlog).hide();
-    let postsToDisplay = [];
-    for (let i = 0; i < realBlog.length; i += 1) {
-      if (i >= page * postsPerPage && i <= page * postsPerPage + postsPerPage - 1) {
-        postsToDisplay.push(realBlog[i]);
-        console.log(i);
-        $(postsToDisplay[i]).show();
+    $realBlog.hide(); //Hide everything on the page
+    let postsToDisplay = []; //Create an empty array to push blog posts into
+    for (let i = 0; i < $realBlog.length; i += 1) { //Looping through each blog post
+      if (i >= page * postsPerPage && i <= page * postsPerPage + postsPerPage - 1) { //Function to get page 1 = posts 1-10, page 2 = posts 11-20, etc.
+        postsToDisplay.push($realBlog[i]); //Push the correct blog posts to the empty array
+        $(postsToDisplay).show(); //Show the array of blog posts on the page
       }
     }
-    console.log(postsToDisplay);
+    return postsToDisplay;
   }
+  showPage(0); //Run the function so when you open up the blog page it starts on page 1
 
   function createPageNumbers() {
-    var createUl = document.createElement("ul");
-    createUl.className = "pageNumbers";
-    for (let i = 1; i <= Math.ceil(realBlog.length/10); i += 1) {
-      var createLi = document.createElement("li");
-      var createA = document.createElement("a");
-      createA.href = "#" + i;
-      createA.textContent = i;
-      createLi.className = "pageButton";
-      createLi.append(createA);
-      createUl.append(createLi);
-      $(".pagination").append(createUl);
-      createA.addEventListener("click", () => {
-        showPage(i - 1);
+    var createUl = document.createElement("ul"); //Declare createUl so every time it's called it'll create an unordered list object
+    createUl.className = "pageNumbers"; //Give the unordered list object a class
+    for (let i = 1; i <= Math.ceil($realBlog.length/10); i += 1) { //Count the amount of blog posts, and for every 10 posts...
+      var createLi = document.createElement("li"); //Create a list item
+      var createA = document.createElement("a"); //Create an anchor item
+      createA.href = "#" + i; //Set the anchor's href to # + i
+      createA.textContent = i; //Set the text content of the anchor to i
+      createLi.className = "pageButton"; //Set the list item's class to "PageButton"
+      createLi.append(createA); //Append the anchor to the list item
+      createUl.append(createLi); //Append the list item to the unordered list
+      $(".pagination").append(createUl); //Append the unordered list to the page element with the class "pagination"
+      createA.addEventListener("click", () => { //Add a click listener to the anchors
+        showPage(i - 1); //Run the showPage function for whatever i is clicked
       });
     }
   }
-
-  showPage(0);
   createPageNumbers();
 });
