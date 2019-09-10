24f0bea6fb6c3cdf65fc4020ef1a7dba5de397d4
neil-c1119 <neil.curtis1119@gmail.com>
2019-09-10 12:11:45 -0700

    Single blog view functional | Some styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   var $realBlog = $(".realBlog");
   var numberOfPosts = $realBlog.length;
-  var postsPerPage = 10;
-  var pageCount = Math.ceil($realBlog.length/10);
+  var postsPerPage = 5;
+  var pageCount = Math.ceil(numberOfPosts/postsPerPage);
   var $pagination = $(".pagination");
+  var $backToBlogs = $(".backToBlogs");
+  var backToBlogs = document.getElementById("backToBlogs");
+  var $blogPageButtons = $(".blogPageButtons");
 
   function showPage(page) {
     $realBlog.hide(); //Hide everything on the page
+    $backToBlogs.hide(); //Hide the "Back to the Blogs" div at the bottom of the page
+    $blogPageButtons.show(); //Show the blogPageButtons in case they were hidden before
     let postsToDisplay = []; //Create an empty array to push blog posts into
     for (let i = 0; i < numberOfPosts; i += 1) { //Looping through each blog post
       if (i >= page * postsPerPage && i <= page * postsPerPage + postsPerPage - 1) { //Function to get page 1 = posts 1-10, page 2 = posts 11-20, etc.
@@ -15,6 +20,7 @@ $(document).ready(function() {
       }
     }
     return postsToDisplay;
+    createPageNumbers(); //Run the createPageNumbers whenever the showPage function is called
   }
   showPage(0); //Run the function so when you open up the blog page it starts on page 1
 
@@ -26,50 +32,45 @@ $(document).ready(function() {
       var createA = document.createElement("a"); //Create an anchor item
       createA.href = "#" + i; //Set the anchor's href to # + i
       createA.textContent = i; //Set the text content of the anchor to i
-      createLi.className = "pageButton"; //Set the list item's class to "PageButton"
+      createLi.className = "pageButton inflate"; //Set the list item's class to "PageButton"
       createLi.append(createA); //Append the anchor to the list item
       createUl.append(createLi); //Append the list item to the unordered list
       $(".pagination").append(createUl); //Append the unordered list to the page element with the class "pagination"
       createA.addEventListener("click", () => { //Add a click listener to the anchors
         showPage(i - 1); //Run the showPage function for whatever i is clicked
-        window.scrollTo(0,0);
+        window.scrollTo(0,0); //Scroll to the top of the page
       });
     }
   }
-  createPageNumbers();
+  createPageNumbers(); //Run the function so it creates page links at the bottom
 
   function individualPost() {
-    let postsToListen = []
-    for (let i = 0; i < numberOfPosts; i++) {
-      postsToListen.push($realBlog[i]);
-      let postToDisplay = [];
-      postsToListen[i].addEventListener("click", () => {
-        $realBlog.hide();
-        postToDisplay.push(postsToListen[i]);
-        $(postToDisplay).show();
-        $(".blogPageButtons").hide();
-        $(".borderBottom").hide();
-        if ($(".backToBlogs").length) {
-          console.log("It exists already!");
-        }
-        else {
-          let createA = document.createElement("a");
-          createA.href = "#";
-          let backToBlogs = document.createElement("p");
-          backToBlogs.innerText = "Back to the blogs";
-          backToBlogs.className = "backToBlogs";
-          $(createA).append(backToBlogs);
-          $(".realBlog").append(createA);
-          createA.addEventListener("click", () => {
-            showPage(0);
-          });
-        }
+    let postsToListen = [] //An empty array for posts to add an eventlistener to
+    for (let i = 0; i < numberOfPosts; i++) { //Loop throught all of the posts on the page and...
+      postsToListen.push($realBlog[i]); //Push each blog post into the postsToListen array
+      let postToDisplay = []; //An empty array for deciding which post to display
+      postsToListen[i].addEventListener("click", () => { //When you click on a specific post...
+        $realBlog.hide(); //Hide every post
+        postToDisplay.push(postsToListen[i]); //Push the clicked post into the postToDisplay array
+        $(postToDisplay).show(); //Show what's in the postToDisplay array
+        $(".blogPageButtons").hide(); //Hide the page buttons at the bottom
+        $(".borderBottom").hide(); //Hide the bottom border of the posts
+        $backToBlogs.show(); //Show the "Back to the Blogs" div at the bottom of the page
+        backToBlogs.addEventListener("click", () => { //When you click on the "Back to the Blogs" div...
+          showPage(0); //Show the first page of blog posts
+        });
       });
     }
   }
-  individualPost();
+  individualPost(); //Run the function so it adds all eventListeners to each post
 
-  function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  if (document.URL.contains("blog.html")) {
+    
+  }
+  elseif () {
+
+  }
+  elseif () {
+
   }
 });
